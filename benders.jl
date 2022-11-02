6dfa9cc9f919e77cd6dce4263c22d36759eb45f6
Matias Vistnes <matias.vistnes@ntnu.no>
2022-11-02 15:58:39 +0100

    Added lsc + repair time + better shed ctrl

--- benders.jl
+++ benders.jl
@@ -1,64 +1,68 @@
 using PowerSystems
 using JuMP
 using Printf
+import MathOptInterface
+const MOI = MathOptInterface
 include("N-1_SCOPF.jl")
 
-function make_first_stage_problem(system::System, optimizer; time_limit_sec = 60, voll = nothing, contingencies = nothing, prob = nothing, unit_commit::Bool=false)
+function benders_scopf(system::System, optimizer; 
+        time_limit_sec::Int64 = 600,
+        voll = nothing, 
+        contingencies = nothing, 
+        unit_commit::Bool = false,
+        max_shed::Float64 = 0.1,
+        ratio::Float64= 0.5, 
+        circuit_breakers::Bool=false,
+        short_term_limit_multi::Float64 = 1.5,
+        ramp_minutes::Int64 = 10,
+        prob = nothing)
     M = -1000
-    voll = isnothing(voll) ? make_voll(system) : voll
-    opfm = opfmodel(system, optimizer, time_limit_sec, voll)
-    opfm = init_var_dc_SCOPF!(opfm) |> set_ref_angle! |> add_c_bus! |> add_c_branch! |> add_obj!
-    if unit_commit
-        opfm = add_unit_commit!(opfm)
-    end
+    opfm = p_scopf(system, optimizer, time_limit_sec=time_limit_sec, voll=voll, contingencies=contingencies, 
+        unit_commit=unit_commit, max_shed=max_shed, ratio=ratio, circuit_breakers=circuit_breakers,
+        short_term_limit_multi=short_term_limit_multi)
     @variable(opfm.mod, θ >= M)
     # @objective(opfm.mod, Min, θ)
     set_objective_function(opfm.mod, objective_function(opfm.mod) + θ)
+    
+    ABSOLUTE_OPTIMALITY_GAP = 1e-6
+    k = 0
+    """ Callback for Benders decomposition. """
+    function benders_cb(cb_data)
+        k += 1
+        pg0_k = callback_value.(cb_data, pg0)
+        ls0_k = callback_value.(cb_data, ls0)
+        θ_k = callback_value(cb_data, θ)
+        lower_bound = voll' * pg0_k + θ_k
+        ret = [solve_subproblem(pg0_k, ls0_k, c) for c in contingencies]
+        upper_bound = voll' * pg0_k + prob * voll' * ret.y
+        gap = (upper_bound - lower_bound) / upper_bound
+        print_iteration(k, lower_bound, upper_bound, gap)
+        if gap < ABSOLUTE_OPTIMALITY_GAP
+            println("Terminating with the optimal solution")
+            return
+        end
+        cut = @build_constraint(θ >= ret.obj + -ret.π' * A_1 * (pg0 .- pg0_k))
+        MOI.submit(model, MOI.LazyConstraint(cb_data), cut)
+        return
+    end
     MOI.set(opfm.mod, MOI.LazyConstraintCallback(), benders_cb)
     solve_model!(opfm.mod)
     return opfm
 end
 
-function solve_subproblem(x, contingency)
-    opfm = opfmodel(system, optimizer, time_limit_sec, voll)
-        # add pgu and pgd here and make pg0 a parameter!!!
-    opfm = init_var_dc_SCOPF!(opfm, max_shed) |> add_c_bus! |> add_c_branch! |> add_obj!
-    if unit_commit
-        opfm = add_unit_commit!(opfm)
-    end
-    @constraint(opfm, opfm[:pf0][contingency] == 0)
+function solve_subproblem(pg0, ls0, contingency, u = nothing)
+    opfm = c_scopf(system, optimizer, pg0, ls0, u, time_limit_sec=time_limit_sec, voll=voll, 
+        contingencies=contingency, unit_commit=unit_commit, max_shed=max_shed, 
+        short_term_limit_multi=short_term_limit_multi, ramp_minutes=ramp_minutes, prob=prob)
     solve_model!(opfm.mod)
     return (
         obj = objective_value(opfm.mod), 
-        # pgu = value.(opfm[:pgu]), 
-        # λ = dual.(opfm[:pfcc_lim]), 
+        pgu = value.(opfm[:pgu]), 
+        λ = dual.(opfm[:pfcc_lim]), 
         lsc = value.(opfm[:lsc]), 
-        π = dual.(opfm[:pfc_lim])
+        π = dual.(opfm[:pfcc_lim])
     )
 end
-
-MAXIMUM_ITERATIONS = 100
-ABSOLUTE_OPTIMALITY_GAP = 1e-6
-k=0
-""" Callback for Benders decomposition. """
-function benders_cb(cb_data, k=0)
-    global k += 1
-    x_k = callback_value.(cb_data, x)
-    θ_k = callback_value(cb_data, θ)
-    lower_bound = c_1' * x_k + θ_k
-    ret = [solve_subproblem(x_k,c) for c in contingencies]
-    upper_bound = c_1' * x_k + c_2' * ret.y
-    gap = (upper_bound - lower_bound) / upper_bound
-    print_iteration(_k, lower_bound, upper_bound, gap)
-    if gap < ABSOLUTE_OPTIMALITY_GAP
-        println("Terminating with the optimal solution")
-        return
-    end
-    cut = @build_constraint(θ >= ret.obj + -ret.π' * A_1 * (x .- x_k))
-    MOI.submit(model, MOI.LazyConstraint(cb_data), cut)
-    return
-end
-
 
 function print_iteration(k, args...)
     f(x) = Printf.@sprintf("%12.4e", x)
--- short_long_SCOPF.jl
+++ short_long_SCOPF.jl
@@ -1,59 +1,81 @@
 using PowerSystems
-const PSY = PowerSystems
 using JuMP
-using Ipopt
-using Printf
 include("N-1_SCOPF.jl")
 
 function sl_scopf(system::System, optimizer; 
-        time_limit_sec = 60,
         voll = nothing, 
         contingencies = nothing, 
+        prob = nothing,
+        time_limit_sec = 600,
         unit_commit::Bool=false,
         max_shed = 0.1,
         ratio = 0.5, 
         short_term_limit_multi = 1.5,
-        ramp_minutes = 10,
-        prob = nothing)
+        ramp_minutes::Int64 = 10,
+        repair_time::Float64 = 1.0)
+    system = set_renewable_prod!(system, ratio)
     voll = isnothing(voll) ? make_voll(system) : voll
     contingencies = isnothing(contingencies) ? get_name.(branches(system)) : contingencies
     prob = isnothing(prob) ? make_prob(contingencies) : prob
     
-    p_opfm = p_scopf(system, optimizer, contingencies=contingencies, 
-        short_term_limit_multi=short_term_limit_multi, voll=voll)
-    solve_model!(p_opfm.mod)
-    
-    set_renewable_prod!(system, ratio)
+    p_opfm = p_scopf(system, optimizer, time_limit_sec=time_limit_sec, voll=voll, contingencies=contingencies, 
+        unit_commit=unit_commit, max_shed=max_shed, ratio=ratio, short_term_limit_multi=short_term_limit_multi, ramp_minutes=ramp_minutes)
+    p_opfm.mod = solve_model!(p_opfm.mod)
+    c_mod = c_scopf(system, optimizer, value.(p_opfm.mod[:pg0]), value.(p_opfm.mod[:ls0]), value.(p_opfm.mod[:lsc]), 
+        unit_commit ? value.(p_opfm.mod[:u]) : nothing, voll=voll, contingencies=contingencies, prob=prob, 
+        time_limit_sec=time_limit_sec, unit_commit=unit_commit, max_shed=max_shed,
+        short_term_limit_multi=short_term_limit_multi, ramp_minutes=ramp_minutes, repair_time=repair_time)
+    c_mod = solve_model!(c_mod)
+
+    return p_opfm, c_mod
+end
+
+function c_scopf(system::System, optimizer, pg0, ls0, lsc, u = nothing; 
+        voll = nothing, 
+        contingencies = nothing,
+        prob = nothing, 
+        time_limit_sec::Int64 = 600,
+        unit_commit::Bool = false,
+        max_shed::Float64 = 0.1,
+        max_curtail::Float64 = 1.0,
+        short_term_limit_multi::Float64 = 1.5,
+        ramp_minutes::Int64 = 10,
+        repair_time::Float64 = 1.0)
+
     model = Model(optimizer)
     
     @variables(model, begin
-        pgu[g in get_name.(get_ctrl_generation(system)), c in contingencies] >= 0    # active power variables for the generators in contingencies ramp up 
-        pgd[g in get_name.(get_ctrl_generation(system)), c in contingencies] >= 0       # and ramp down
+        0 <= pgu[g in get_name.(get_ctrl_generation(system)), c in contingencies]    # active power variables for the generators in contingencies ramp up 
+        0 <= pgd[g in get_name.(get_ctrl_generation(system)), c in contingencies]       # and ramp down
         pfcc[l in get_name.(branches(system)), c in contingencies]         # and after corrective actions
         vacc[b in get_name.(nodes(system)), c in contingencies]            # and after corrective actions
-        lsc[d in get_name.(get_nonctrl_generation(system)), c in contingencies] >= 0 # load curtailment variables in in contingencies
+        0 <= lscc[d in get_name.(get_nonctrl_generation(system)), c in contingencies] # load curtailment variables in in contingencies
     end)
 
     # minimize socio-economic cost
     @objective(model, Min, 
-        sum(get_generator_cost(g) * (value.(p_opfm.mod[:pg0][get_name(g)]) + 
+        sum(get_generator_cost(g)[2] * (pg0[get_name(g)] + 
             sum(prob[c] * (pgu[get_name(g),c] #=+ pgd[get_name(g),c]*0.1=#) for c in contingencies))
             for g in gens_t(system)
         ) + 
-        sum(5 * (value.(p_opfm.mod[:pg0][get_name(g)]) + sum(prob[c] * (pgu[get_name(g),c] #=+ pgd[get_name(g),c]*0.1=#)
+        sum(5 * (pg0[get_name(g)] + sum(prob[c] * (pgu[get_name(g),c] #=+ pgd[get_name(g),c]*0.1=#)
             for c in contingencies)) for g in gens_h(system)
         ) +
-        sum(voll[d] * (value.(p_opfm.mod[:ls0][d]) + sum(prob[c] * lsc[d,c] for c in contingencies)) 
+        sum(voll[d] * (ls0[d] + sum(prob[c] * (lsc[d,c] * ramp_minutes / 60 + lscc[d,c] * repair_time) for c in contingencies)) 
             for d in get_name.(get_nonctrl_generation(system))
         )
     )
+    if unit_commit
+        set_objective_function(model, objective_function(model) + 
+        sum(u[get_name(g)] * get_generator_cost(g)[end] for g in gens_t(system)))
+    end
 
     # incerted power at each bus for the base case and contingencies
     @constraint(model, inj_pcc[n = get_name.(nodes(system)), c = contingencies],
-        sum(get_name(get_bus(g)) == n ? value(p_opfm.mod[:pg0][get_name(g)]) + pgu[get_name(g),c] - pgd[get_name(g),c] : 0 for g in get_ctrl_generation(system)) -
+        sum(get_name(get_bus(g)) == n ? pg0[get_name(g)] + pgu[get_name(g),c] - pgd[get_name(g),c] : 0 for g in get_ctrl_generation(system)) -
         sum(beta(n,l) * pfcc[get_name(l),c] for l in branches(system)) == 
-        sum(get_name(get_bus(d)) == n ? get_active_power(d) - lsc[get_name(d),c] : 0 for d in demands(system)) +
-        sum(get_name(get_bus(d)) == n ? -get_active_power(d) + lsc[get_name(d),c] : 0 for d in renewables(system))
+        sum(get_name(get_bus(d)) == n ? get_active_power(d) - lscc[get_name(d),c] : 0 for d in demands(system)) +
+        sum(get_name(get_bus(d)) == n ? -get_active_power(d) + lscc[get_name(d),c] : 0 for d in renewables(system))
     )
     # @constraint(model, -π/2 .<= vacc .<= π/2) # Not really needed, could be implemented with spesific line angle limits
     
@@ -71,20 +93,38 @@ function sl_scopf(system::System, optimizer;
     )
 
     # restrict active power generation to min and max values
-    for g in get_ctrl_generation(system)
+    for g in gens_t(system)
         g_name = get_name(g)
         for c in contingencies
-            @constraint(model, 0 <= value.(p_opfm.mod[:pg0][g_name]) + (pgu[g_name,c] - pgd[g_name,c]) <= get_active_power_limits(g).max)
+            if unit_commit
+                @constraint(model, 0 <= pg0[g_name] + (pgu[g_name,c] - pgd[g_name,c]) <= 
+                    get_active_power_limits(g).max * u[g_name])
+            else
+                @constraint(model, 0 <= pg0[g_name] + (pgu[g_name,c] - pgd[g_name,c]) <= 
+                    get_active_power_limits(g).max)
+            end
+            set_upper_bound(pgu[g_name,c], get_ramp_limits(g).up * ramp_minutes)
+            set_upper_bound(pgd[g_name,c], get_ramp_limits(g).down * ramp_minutes)
+        end
+    end
+    for g in gens_h(system)
+        g_name = get_name(g)
+        for c in contingencies
+            @constraint(model, 0 <= pg0[g_name] + (pgu[g_name,c] - pgd[g_name,c]) <= 
+                    get_active_power_limits(g).max)
             set_upper_bound(pgu[g_name,c], get_ramp_limits(g).up * ramp_minutes)
             set_upper_bound(pgd[g_name,c], get_ramp_limits(g).down * ramp_minutes)
         end
     end
 
     # restrict load shedding to load per node
-    for l in get_nonctrl_generation(system), c in contingencies
-        @constraint(model, value.(p_opfm.mod[:ls0][get_name(l)]) + lsc[get_name(l),c] <= get_active_power(l))
+    for l in demands(system), c in contingencies
+        @constraint(model, lscc[get_name(l),c] <= get_active_power(l) * max_shed)
+    end
+    # restrict renewable shedding to renewable production
+    for l in renewables(system), c in contingencies
+        @constraint(model, lscc[get_name(l),c] <= get_active_power(l) * max_curtail)
     end
 
-    solve_model!(model)
-    return p_opfm, model
+    return model
 end
